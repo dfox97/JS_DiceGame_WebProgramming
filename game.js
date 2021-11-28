04cbf2e8e8af7a9f533b2004e6613b820f69f649
Daniel Fox <61083107+dfox97@users.noreply.github.com>
2021-11-28 20:36:19 +0000

    Fixed/Updated dice bug
    Fixed dice to hide and display using the display:hidden and visable.
    Cleaned up code by adding functions to reduce repeated code.
    
    Todo:
    - Add 
    The program then provides the user with the options (i) to continue with another round, or (ii) to end the game. If option (i) is chosen, then the program remains in the play stage and proceeds with another round. If option (ii) is chosen, then the program proceeds to the end stage.
    
    In the end stage, the program computes the average number of points won per round and displays
    
    the number of rounds played,
    the balance of points when reaching the end stage,
    the average number of points won per round played, rounded to one digit after the decimal point.

--- game.js
+++ game.js
@@ -1,1 +1,237 @@
-function newGame(){totalScore=0,RoundNum=0,document.getElementById("TotalScore").innerHTML=0,document.getElementById("roundScore").innerHTML=0,document.getElementById("roundNum").innerHTML=0,document.getElementById("dieContainer").innerHTML=0,die1=document.getElementById("dice").src="dice-1.png",die2=document.getElementById("dice1").src="dice-1.png",die3=document.getElementById("dice2").src="dice-1.png",document.getElementById("alert").innerHTML=""}function rollDice(){numDice=document.getElementById("diceNum").value;var e=document.getElementById("dieContainer"),n=[],t=0;if(3>numDice||numDice>6)return window.alert("Please enter 3 to 6 dice"),t=0,RoundNum=0;e.innerHTML="";for(var i=0;numDice>i;i++)dice=Math.floor(6*Math.random()+1),n.push(dice),console.log(n[0]),t+=dice;numDice>=3&&(RoundNum+=1,document.getElementById("roundNum").innerHTML=RoundNum,die1=document.getElementById("dice").src="dice-"+n[0]+".png",die2=document.getElementById("dice1").src="dice-"+n[1]+".png",die3=document.getElementById("dice2").src="dice-"+n[2]+".png",numDice>=4&&(die4=document.getElementById("dice3").style.display="inline",die4=document.getElementById("dice3").src="dice-"+n[3]+".png"),numDice>=5&&(die5=document.getElementById("dice4").style.display="inline",die5=document.getElementById("dice4").src="dice-"+n[4]+".png"),6==numDice&&(die6=document.getElementById("dice5").style.display="inline",die6=document.getElementById("dice5").src="dice-"+n[5]+".png")),1==identical(n)&&(t+=60,console.log("Identical: "+t),document.getElementById("roundScore").innerHTML=t,document.getElementById("alert").innerHTML="Identical!!!"),1==identicalMinusOne(n)&&(t+=40,console.log("N-1: "+t),document.getElementById("roundScore").innerHTML=t,document.getElementById("alert").innerHTML="40 points !!"),1==isStraight(n)&&(t+=20,console.log("Straight: "+t),document.getElementById("roundScore").innerHTML=t,document.getElementById("alert").innerHTML="Straight!!"),1==allUniqueValues(n)&&(t+=0,console.log("All Unqiue and not a Straight: "+t),document.getElementById("roundScore").innerHTML=t,document.getElementById("alert").innerHTML="Unique!"),1==otherOutcome(n)&&(t=0,console.log("Other outcome: "+t),document.getElementById("roundScore").innerHTML=t,document.getElementById("alert").innerHTML=""),totalScore+=t,document.getElementById("TotalScore").innerHTML=totalScore}function identical(e){for(var n=e[0],t=0;t<e.length;t++)if(e[t]!==n)return!1;return!0}function identicalMinusOne(e){e.sort();for(var n=null,t=0,i=0;i<=e.length;i++)if(e[i]!=n){if(t>0&&t===e.length-1)return!0;n=e[i],t=1}else t++;return!1}function isStraight(e){e.sort(function(e,n){return e-n});for(var n=1;n<e.length;n++){var t=e[n-1],i=e[n];if(t+1!==i)return!1}return!0}function allUniqueValues(e){for(var n=0;n<e.length&&1!=isStraight(e)&&1!=identicalMinusOne(e)&&1!=identical(e);n++)if(e.length===new Set(e).size)return!0;return!1}function otherOutcome(e){return isStraight(e)||identicalMinusOne(e)||identical(e)||allUniqueValues(e)===!0?!1:!0}function setup(){window.alert("Please select between 3-6 dice and roll, in each round a total score will be calcualted depending on the sequence of numbers. Each roll will count as 1 round, if you want to start again press New Game")}var totalScore=0,RoundNum=0;
+//global variables
+var totalScore = 0,
+  RoundNum = 0;
+
+//RESETS GAME
+function newGame() {
+  //add these so total gets reset to zero if it doesnt then it will continue adding
+  (totalScore = 0), (RoundNum = 0);
+  document.getElementById("TotalScore").innerHTML = 0;
+  document.getElementById("roundScore").innerHTML = 0;
+  document.getElementById("roundNum").innerHTML = 0;
+  document.getElementById("dieContainer").innerHTML = 0;
+  document.getElementById("alert").style.visibility = "hidden";
+}
+
+//generates 5 random numbers between 1 and 6
+function rollDice() {
+  document.getElementById("alert").style.visibility = "visible";
+  numDice = document.getElementById("diceNum").value;
+  var container = document.getElementById("dieContainer");
+  var diceRoll = [];
+  var roundPoints = 0;
+
+  if (numDice < 3 || numDice > 6) {
+    window.alert("Please enter 3 to 6 dice");
+    return (roundPoints = 0), (RoundNum = 0);
+  } else {
+    container.innerHTML = ""; //clears the old dice rolls
+    for (var i = 0; i < numDice; i++) {
+      dice = Math.floor(Math.random() * 6 + 1);
+      diceRoll.push(dice);
+      roundPoints += dice;
+    }
+  }
+
+  //updating images depending on number of dice slected
+  if (numDice >= 3) {
+    RoundNum += 1;
+    document.getElementById("roundNum").innerHTML = RoundNum;
+
+    displayDice(["dice", "dice1", "dice2"], diceRoll);
+    hideDice(["dice3", "dice4", "dice5"]);
+    if (numDice >= 4) {
+      addDice("dice3", diceRoll[3]);
+      hideDice(["dice4", "dice5"]);
+    }
+    if (numDice >= 5) {
+      addDice("dice4", diceRoll[4]);
+      hideDice(["dice5"]);
+    }
+    if (numDice == 6) addDice("dice5", diceRoll[5]);
+  }
+
+  if (identical(diceRoll) == true) scores(60, roundPoints, "Identical!!!");
+
+  if (identicalMinusOne(diceRoll) == true)
+    scores(40, roundPoints, "40 points !!");
+
+  if (isStraight(diceRoll) == true) scores(20, roundPoints, "Straight");
+
+  if (allUniqueValues(diceRoll) == true) scores(0, roundPoints, "Unique!");
+
+  if (otherOutcome(diceRoll) == true) scores(0, roundPoints, "hidden");
+
+  totalScore += roundPoints;
+  document.getElementById("TotalScore").innerHTML = totalScore;
+}
+
+/************************************************** */
+//Checking  All N dice have the same value
+/************************************************** */
+function identical(array) {
+  var first = array[0];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== first) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/************************************************** */
+//N - 1 but not N dice have the same value
+/************************************************** */
+
+//Counts thee number of elements in array and if the counter is equal the array return true
+function identicalMinusOne(array) {
+  array.sort();
+  var array_increment = null;
+  var count = 0;
+  for (var i = 0; i <= array.length; i++) {
+    if (array[i] != array_increment) {
+      if (count > 0) {
+        if (count === array.length - 1) {
+          return true;
+        }
+      }
+      array_increment = array[i];
+      count = 1;
+    } else {
+      count++;
+    }
+  }
+  return false;
+}
+
+/************************************************** */
+//A run(a sequence K+1 to K+N for some K = 0)
+/************************************************** */
+function isStraight(array) {
+  //sorts array from lowest to highest
+  array.sort(function (a, c) {
+    return a - c;
+  });
+  for (var i = 1; i < array.length; i++) {
+    var previous = array[i - 1];
+    var current = array[i];
+    if (previous + 1 !== current) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/************************************************** */
+//All dice have different values, but it is not a run
+/************************************************** */
+function allUniqueValues(array) {
+  for (var i = 0; i < array.length; i++) {
+    //  var diceValue = array[i];
+    if (isStraight(array) == true) {
+      break;
+    }
+    if (identicalMinusOne(array) == true) {
+      break;
+    }
+    if (identical(array) == true) {
+      break;
+    } else if (array.length === new Set(array).size) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function otherOutcome(array) {
+  if (
+    isStraight(array) ||
+    identicalMinusOne(array) ||
+    identical(array) ||
+    allUniqueValues(array) === true
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/************************************************** */
+// Display and compute round scores
+/************************************************** */
+function scores(N, roundPoints, alert) {
+  if (alert === "hidden") {
+    roundPoints = 0;
+    document.getElementById("roundScore").innerHTML = roundPoints;
+    document.getElementById("alert").style.visibility = "hidden";
+  } else {
+    roundPoints += N;
+    document.getElementById("roundScore").innerHTML = roundPoints;
+    document.getElementById("alert").innerHTML = alert;
+  }
+}
+
+/************************************************** */
+// SHOW HIDE ADD DICE
+/************************************************** */
+function hideDice(eleID) {
+  for (let i = 0; i < eleID.length; i++) {
+    diceNumm = document.getElementById(eleID[i]).style.display = "none";
+    diceNumm = document.getElementById(eleID[i]).src = "";
+  }
+  return diceNumm;
+}
+function displayDice(eleDice, diceRoll) {
+  for (let i = 0; i < eleDice.length; i++) {
+    document.getElementById(eleDice[i]).src = "dice-" + diceRoll[i] + ".png";
+  }
+}
+
+function addDice(dice, N) {
+  die = document.getElementById(dice).style.display = "inline";
+  die = document.getElementById(dice).src = "dice-" + N + ".png";
+  return die;
+}
+function setup() {
+  window.alert(
+    "Please select between 3-6 dice and roll, in each round a total score will be calcualted depending on the sequence of numbers. Each roll will count as 1 round, if you want to start again press New Game"
+  );
+}
+
+//Depreciated Old code //
+
+// if (identical(diceRoll) == true) {
+//     roundPoints += 60;
+//     document.getElementById("roundScore").innerHTML = roundPoints;
+//     document.getElementById("alert").innerHTML = "Identical!!!";
+
+//   }
+
+//   if (identicalMinusOne(diceRoll) == true) {
+
+//     roundPoints += 40;
+//     document.getElementById("roundScore").innerHTML = roundPoints;
+//     document.getElementById("alert").innerHTML = "40 points !!";
+//   }
+
+//   if (isStraight(diceRoll) == true) {
+
+//     roundPoints += 20;
+//     document.getElementById("roundScore").innerHTML = roundPoints;
+//     document.getElementById("alert").innerHTML = "Straight!!";
+//   }
+//   if (allUniqueValues(diceRoll) == true) {
+
+//     roundPoints += 0;
+//     document.getElementById("roundScore").innerHTML = roundPoints;
+//     document.getElementById("alert").innerHTML = "Unique!";
+//   }
+//   if (otherOutcome(diceRoll) == true) {
+
+//     roundPoints = 0;
+//     document.getElementById("roundScore").innerHTML = roundPoints;
+//     document.getElementById("alert").style.visibility = "hidden";
+//   }
+//   totalScore += roundPoints;
+//   document.getElementById("TotalScore").innerHTML = totalScore;
+// }
